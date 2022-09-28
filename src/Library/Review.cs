40e1e06a474bbed54660095e925e3bfe890cba00
NicorRodriguez <nicolas@nicolass-MacBook-Air-579.local>
2022-09-28 20:23:17 -0300

    Added Review class

---
+++ src/Library/Review.cs
@@ -0,0 +1,12 @@
+namespace library;
+
+public class Review{
+    private int stars {get; set;}
+    private string comment {get; set;}
+
+    public Review(string MyComment, int MyStars){
+        this.stars = MyStars;
+        this.comment = MyComment;
+    }
+
+}
