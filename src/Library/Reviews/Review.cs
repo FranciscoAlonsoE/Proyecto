bf5ef6d5137c3e5b23015f72b8c5a8a4a21ad1bb
JavierM1203 <javier.moreno@correo.ucu.edu.uy>
2022-10-30 17:39:24 -0300

    Doxygens faltantes en Review y Review Controller

--- src/Library/Reviews/Review.cs
+++ src/Library/Reviews/Review.cs
@@ -3,12 +3,34 @@ namespace Library;
 /// Clase Review encargada de crear las reviews.
 /// </summary>
 public class Review{
-    
+    /// <summary>
+    /// Contador de ID. Incrementa en uno cada vez que se crea una review.
+    /// </summary>
     private static int idCounter = 1;
+    /// <summary>
+    /// ID asignada a la variable creada.
+    /// </summary>
+    /// <value></value>
     public int id {get; private set;}
+    /// <summary>
+    /// Usuario que publica la review.
+    /// </summary>
+    /// <value></value>
     public User from {get; private set;}
+    /// <summary>
+    /// Usuario calificado en la review.
+    /// </summary>
+    /// <value></value>
     public User to {get; private set;}
+    /// <summary>
+    /// Cantidad de estrellas otorgadas al usuario calificado.
+    /// </summary>
+    /// <value></value>
     public int stars {get; private set;}
+    /// <summary>
+    /// Comentario de la review.
+    /// </summary>
+    /// <value></value>
     public string comment {get; private set;}
 
     /// <summary>
